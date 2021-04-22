8cde8467517489af0b0cf0afe08be5cbf7a7679c
Hongli Wang <1023836798@qq.com>
2021-04-22 16:02:56 -0400

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import products from './data/products.js'
+import connectDB from './config/db.js'
+
 const app = express()
 dotenv.config()
+
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('API is running')
