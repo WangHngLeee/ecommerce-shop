30869e88d96cc3b24aac8bec5e9876fbddd8035b
Hongli Wang <1023836798@qq.com>
2021-04-22 14:07:52 -0400

    Add product list page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+
 import { Container } from 'react-bootstrap'
 const App = () => {
   return (
@@ -7,7 +9,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>welcome</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className='rating'>
+      <span>
+        {[1, 2, 3, 4, 5].map((index) => (
+          <i
+            style={{ color: color }}
+            className={
+              value >= index
+                ? 'fas fa-star'
+                : value >= index - 0.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+            }
+          ></i>
+        ))}
+      </span>
+      <span>{text && text}</span>
+    </div>
+  )
+}
+
+Rating.defaultProps = {
+  color: '#F7DC6F',
+}
+
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string,
+  color: PropTypes.string,
+}
+export default Rating
