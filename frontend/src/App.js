06eec914160024c0962332d7a5d177a531fd0a15
Hongli Wang <1023836798@qq.com>
2021-04-22 12:14:43 -0400

    Add basic footer and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
 const App = () => {
-  return <h1>welcome</h1>
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>welcome</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Row, Col, Container } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; SimpShop</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
