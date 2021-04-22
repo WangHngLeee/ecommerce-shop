15321e082cf92c8fccde8f349dc14f9df7e2edb6
Hongli Wang <1023836798@qq.com>
2021-04-22 18:21:33 -0400

    Add Reducer and actions for productlist

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRouter from './routes/productRoute.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 const app = express()
 dotenv.config()
@@ -13,6 +14,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRouter)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const port = process.env.PORT || 5000
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,9 +4,9 @@ import Rating from './Rating'
 import { Link } from 'react-router-dom'
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className='my-3 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <Card.Img src={product.image} variant='top' fluid />
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
