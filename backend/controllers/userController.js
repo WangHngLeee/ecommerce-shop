6ba5951445c1e1af5f4b2e83cfc1bb40a126a694
Hongli Wang <1023836798@qq.com>
2021-04-23 15:29:23 -0400

    Add backend logic for login & register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,8 +3,8 @@ import User from '../models/UserModel.js'
 import generateToken from '../utils/generateToken.js'
 
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body
-  const user = await User.findOne({ email: email })
+  const { name, email, password } = req.body
+  const userExists = await User.findOne({ email })
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
@@ -16,6 +16,32 @@ const authUser = asyncHandler(async (req, res) => {
   } else {
     res.status(401)
     throw new Error('Invalid email or password')
+  }
+})
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid data')
   }
 })
 
@@ -35,4 +61,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
