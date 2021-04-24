561b90b759e9260ecfbe2b94e5a1e6e84408ff59
Hongli Wang <1023836798@qq.com>
2021-04-24 15:22:45 -0400

    Finished order details screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
-
+import OrderScreen from './screens/OrderScreen'
 const App = () => {
   return (
     <Router>
@@ -27,6 +27,7 @@ const App = () => {
           <Route path='/shipping' component={ShippingScreen} />
           <Route path='/payment' component={PaymentScreen} />
           <Route path='/placeorder' component={PlaceOrderScreen} />
+          <Route path='/order/:id' component={OrderScreen} />
         </Container>
       </main>
       <Footer />
