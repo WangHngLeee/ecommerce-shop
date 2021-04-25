e394af4306d1760927345a3ae18c4071992aca76
Hongli Wang <1023836798@qq.com>
2021-04-25 15:49:17 -0400

    Add routes for update/create product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -26,4 +26,56 @@ const deleteProduct = asyncHandler(async (req, res) => {
     throw new Error('Product not found')
   }
 })
-export { getProductById, getProducts, deleteProduct }
+
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'simple name',
+    price: 12,
+    user: req.user._id,
+    image: 'images/sample.jpg',
+    brand: 'simple brand',
+    category: 'asdf',
+    countInStock: 12,
+    numReviews: 2,
+    description: 'test product asdfasfdafds',
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body
+
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.status(201).json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+export {
+  getProductById,
+  getProducts,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+}
